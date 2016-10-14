2325406751c78fc6e49879269af0362d0d5d4747
Prasad Chandrashekar <prasad.chandrashekar@reachlocal.com>
2016-10-14 21:22:56 +0530

    Adding react-native-maps.

--- app/components/pickups.ios.js
+++ app/components/pickups.ios.js
@@ -6,24 +6,45 @@ import {
   DatePickerIOS
 } from 'react-native';
 
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
 import Branding from './branding.ios.js';
 
 export default class PickUps extends Component {
 
   render () {
     return (
-      <View style={styles.outer}>
-        <Branding/>
-        <View style={{flex: 0.75}}>
-        </View>
+      <View style={styles.container}>
+          <MapView
+            provide={PROVIDER_GOOGLE}
+            style={styles.map}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
       </View>
     );
   } 
 }
 
 const styles = StyleSheet.create({
-  outer: {
-    flex: 1,
-    backgroundColor: '#c6d6f3'
-  }
+  container: {
+      position: 'absolute',
+      top: 40,
+      left: 20,
+      right: 20,
+      bottom: 40,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      position: 'absolute',
+      top: 100,
+      left: 0,
+      right: 0,
+      bottom: 150,
+    },
 });
